9ea128c0f241302000903a5094a72528b56c552d
Etienne Zink <etienne-zink@gmx.de>
2021-06-20 12:24:18 +0200

    Added popup window.

---
+++ Client/Controllers/LoginWindowController.cs
@@ -0,0 +1,48 @@
+﻿using Autofac;
+using Baka.Hipster.Burger.Client.Framework;
+using Baka.Hipster.Burger.Client.ViewModels;
+using Baka.Hipster.Burger.Client.Views;
+using Baka.Hipster.Burger.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media;
+
+namespace Baka.Hipster.Burger.Client.Controllers
+{
+    public class LoginWindowController
+    {
+        private readonly LoginWindow _loginWindow;
+        private readonly LoginWindowViewModel _loginWindowViewModel;
+        private readonly App _app;
+
+        private string Token { get; set; }
+        private User User { get; set; }
+        //add userserviceclient
+        public LoginWindowController(LoginWindow loginWindow, LoginWindowViewModel loginWindowViewModel, App app)
+        {
+            _loginWindow = loginWindow;
+            _loginWindowViewModel = loginWindowViewModel;
+            _loginWindowViewModel.LoginCommand = new RelayCommand(ExecuteLoginCommand);
+            
+            _app = app;
+            _app.MainWindow = _loginWindow;
+            _loginWindow.DataContext = _loginWindowViewModel;
+        }
+
+        public void ExecuteLoginCommand(object o)
+        {
+            var _popupWindowController = _app.Container.Resolve<PopupWindowController>();
+            _popupWindowController.DisplayText("The username or password was wrong. Please try again!");
+        }
+
+        public void Initialize()
+        {
+            _loginWindow.ShowDialog();
+        }
+    }
+}
---
+++ Client/ViewModels/LoginWindowViewModel.cs
@@ -0,0 +1,41 @@
+﻿using Baka.Hipster.Burger.Client.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Baka.Hipster.Burger.Client.ViewModels
+{
+    public class LoginWindowViewModel: ViewModelBase
+    {
+        private string _userName;
+
+        public string Username
+        {
+            get => _userName;
+            set
+            {
+                if (_userName == value) return;
+                _userName = value;
+                OnPropertyChanged(nameof(_userName));
+            }
+        }
+
+        private string _password;
+
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                if (_password == value) return;
+                _password = value;
+                OnPropertyChanged(nameof(_password));
+            }
+        }
+
+        public ICommand LoginCommand { get; set; }
+    }
+}
