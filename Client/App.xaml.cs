820e9be3d4cfc0ee6824d1f8f447d2ae2ca73a59
Etienne Zink <etienne-zink@gmx.de>
2021-06-20 10:49:41 +0200

    Updated login and added example classes from old project.

--- Client/App.xaml.cs
+++ Client/App.xaml.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Autofac;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,5 +15,19 @@ namespace Baka.Hipster.Burger.Client
     /// </summary>
     public partial class App : Application
     {
+        public IContainer Container { get; set; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            ContainerBuilder containerBuilder = new ContainerBuilder();
+            containerBuilder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly())
+                .Where(t => t.IsClass && (t.Namespace.Contains("Controllers") || t.Namespace.Contains("ViewModels") || t.Namespace.Contains("Views")));
+            containerBuilder.RegisterInstance(this);
+            //ToDo
+            //containerBuilder.RegisterType<CustomerServiceClient>();
+            Container = containerBuilder.Build();
+
+            //Container.Resolve<MainWindowController>().Initialize();
+        }
     }
 }
---
+++ Client/Framework/ViewModelBase.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Baka.Hipster.Burger.Client.Annotations;
+
+namespace Baka.Hipster.Burger.Client.Framework
+{
+    public abstract class ViewModelBase: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
