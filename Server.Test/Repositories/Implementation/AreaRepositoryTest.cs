5a94c504db979a8f3460b5652f45f5b35561fe4b
Etienne Zink <etienne-zink@gmx.de>
2021-06-27 18:17:31 +0200

    Updated tests.

---
+++ Server.Test/Repositories/Implementation/AreaRepositoryTest.cs
@@ -0,0 +1,42 @@
+﻿using Baka.Hipster.Burger.Server.Helper.Interfaces;
+using Baka.Hipster.Burger.Server.Repositories.Implementation;
+using Baka.Hipster.Burger.Shared.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using NHibernate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baka.Hipster.Burger.Server.Test.Repositories.Implementation
+{
+    [TestClass]
+    public class AreaRepositoryTest
+    {
+        //ranking5
+        [TestMethod]
+        public async Task GetAll_ThrowsException_Fails()
+        {
+            var sessionMock = new Mock<ISession>();
+            sessionMock
+                .Setup(s => s.QueryOver<Area>())
+                .Throws(new Exception());
+
+            var nhibernateHelperMock = new Mock<INHibernateHelper>();
+            nhibernateHelperMock
+                .Setup(n => n.OpenSession())
+                .Returns(sessionMock.Object);
+
+
+            var areaRepository = new AreaRepository(nhibernateHelperMock.Object);
+            var result = await areaRepository.GetAll();
+
+
+            sessionMock.VerifyAll();
+            nhibernateHelperMock.VerifyAll();
+            Assert.IsNull(result);
+        }
+    }
+}
--- Server.Test/Services/OrderServiceTests.cs
+++ Server.Test/Services/OrderServiceTests.cs
@@ -1,5 +1,9 @@
-﻿using Baka.Hipster.Burger.Server.Services;
+﻿using Baka.Hipster.Burger.Server.Repositories.Interfaces;
+using Baka.Hipster.Burger.Server.Services;
+using Baka.Hipster.Burger.Shared.Models;
+using Baka.Hipster.Burger.Shared.Protos;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,6 +23,54 @@ namespace Baka.Hipster.Burger.Server.Test.Services
             var result = await orderService.Delete(null, null);
 
 
+            Assert.IsFalse(result.Result);
+        }
+
+        //manageOrders2
+        [TestMethod]
+        public async Task Delete_OrderNull_Fails()
+        {
+            var idMock = 1;
+            var request = new IdMessage { Id = idMock };
+
+            var orderRepository = new Mock<IOrderRepository>();
+            orderRepository
+                .Setup(m => m.Get(idMock))
+                .ReturnsAsync((Order) null);
+
+
+            var orderService = new OrderService(orderRepository.Object, null, null, null);
+            var result = await orderService.Delete(request, null);
+
+
+            orderRepository.VerifyAll();
+            Assert.IsFalse(result.Result);
+        }
+
+        //manageOrders3
+        [TestMethod]
+        public async Task Delete_OrderLinesNull_Fails()
+        {
+            var idMock = 1;
+            var request = new IdMessage { Id = idMock };
+
+            var orderRepository = new Mock<IOrderRepository>();
+            orderRepository
+                .Setup(m => m.Get(idMock))
+                .ReturnsAsync(new Order());
+
+            var orderLineRepository = new Mock<IOrderLineRepository>();
+            orderLineRepository
+                .Setup(m => m.GetAll())
+                .ReturnsAsync((ICollection<OrderLine>) null);
+
+
+            var orderService = new OrderService(orderRepository.Object, null, null, orderLineRepository.Object);
+            var result = await orderService.Delete(request, null);
+
+
+            orderRepository.VerifyAll();
+            orderLineRepository.VerifyAll();
             Assert.IsFalse(result.Result);
         }
     }
